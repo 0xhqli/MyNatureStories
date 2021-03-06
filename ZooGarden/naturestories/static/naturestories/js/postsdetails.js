c315995637e165ba741702a5097b10b1da408c9e
Hai Qi <53511724+0xhqli@users.noreply.github.com>
2021-03-06 00:49:58 -0800

    comment order and ajax

--- ZooGarden/naturestories/static/naturestories/js/postsdetails.js
+++ ZooGarden/naturestories/static/naturestories/js/postsdetails.js
@@ -1,56 +1,53 @@
 $('document').ready(function(){
     attach()
+    reattach()
 })
 
 function attach(){
-    $('.zones').click(function(link){
-        link.preventDefault()
-        let loc=$(this).attr('loclink')
-        console.log(loc);
-        let ifbit=($(this).attr('active')=='1')
-        console.log(ifbit)
-        if(!ifbit){
-            $('.zones').attr('active','0')
-            $(this).attr('active','1')
-            let form={}
-            form['csrfmiddlewaretoken']=csrf
-            form['zone']=loc
-            console.log(form);
-            $.ajax({
-                type: "POST",
-                url: '/naturestories/ajax_zone_stories',
-                data: form,
-                success: $.proxy(function (result) {
-                    // console.log(result)
-                    $('#posts').fadeOut(150)
-                    setTimeout(function(){
-                        $('#posts').html(result)
-                    }, 150)
-                    $('#posts').fadeIn(150)
-                }, this)
-            });
-        }
-    })
-    $('#search_button').click(function(but){
-        but.preventDefault()
-        let search=$("#search").val()
-        console.log(search);
-        let form={}
-        form['csrfmiddlewaretoken']=csrf
-        form['search']=search
-        console.log(form);
-        $('.zones').attr('active','0')
+    $('#new_comment').submit(function(form){
+        form.preventDefault()
+        let data=$(this).serialize()
+        console.log(data)
         $.ajax({
             type: "POST",
-            url: '/naturestories/ajax_search_stories',
-            data: form,
+            url: '/naturestories/new_comment_ajax',
+            data: data,
             success: $.proxy(function (result) {
                 // console.log(result)
-                $('#posts').fadeOut(150)
-                setTimeout(function(){
-                    $('#posts').html(result)
-                }, 150)
-                $('#posts').fadeIn(150)
+                $('.textarea').val('')
+                $('.textinput').val('')
+                $('#comment_list').html(result)
+                reattach()
+                // $('#comment_list').html(result)
+                // reattach()
+                // $('#posts').fadeOut(150)
+                // setTimeout(function(){
+                //     $('#comment_list').html(result)
+                // }, 150)
+                // $('#posts').fadeIn(150)
+            }, this)
+        });
+    })
+}
+
+function reattach(){
+    $('.reply_form_ajax').submit(function(form){
+        form.preventDefault()
+        let data=$(this).serialize()
+        console.log(data)
+        $.ajax({
+            type: "POST",
+            url: '/naturestories/new_reply_ajax',
+            data: data,
+            success: $.proxy(function (result) {
+                // console.log(result)
+                $('#comment_list').html(result)
+                reattach()
+                // $('#posts').fadeOut(150)
+                // setTimeout(function(){
+                //     $('#comment_list').html(result)
+                // }, 150)
+                // $('#posts').fadeIn(150)
             }, this)
         });
     })
